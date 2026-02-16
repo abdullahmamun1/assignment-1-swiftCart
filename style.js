dc6e5d84b161718eaf602698663da76fa6ae0de3
abdullahmamun1 <hello.abdullahmamun1@gmail.com>
2026-02-16 20:27:35 +0600

    spinner and product by category added

--- style.js
+++ style.js
@@ -1,8 +1,59 @@
+const removeActive = () => {
+    const categoryButtons = document.querySelectorAll('.cat-btn');
+    categoryButtons.forEach(btn => {
+        btn.classList.remove('active');
+    });
+}
+
+const manageSpinner = (status) => {
+    if (status == true) {
+        document.getElementById('spinner').classList.remove('hidden');
+        document.getElementById('product-container').classList.add('hidden');
+    } else {
+        document.getElementById('spinner').classList.add('hidden');
+        document.getElementById('product-container').classList.remove('hidden');
+    }
+}
+
 const loadProducts = async () => {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
   displayProducts(data);
 };
+
+const loadCategories = async () => {
+    const res = await fetch("https://fakestoreapi.com/products/categories");
+    const data = await res.json();
+    displayCategories(data);
+}
+
+const loadProductsByCategory = async (category) => {
+    removeActive();
+    manageSpinner(true);
+    if(category === "All") {
+        loadProducts();
+        const clickBtn = document.getElementById(`cat-btn-All`);
+        clickBtn.classList.add('active');
+        return;
+    }
+    const res = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+    const data = await res.json();
+    displayProducts(data);
+    const clickBtn = document.getElementById(`cat-btn-${category}`);
+    clickBtn.classList.add('active');
+}
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("category-container");
+    categoryContainer.innerHTML = `<button id = "cat-btn-All" onclick = 'loadProductsByCategory("All")' class='btn btn-outline rounded-3xl py-2 px-4 cat-btn active'>All</button>`;
+    categories.forEach(category => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <button id = "cat-btn-${category}" onclick = "loadProductsByCategory('${category.replace("'", "\\'")}')" class="btn btn-outline rounded-3xl py-2 px-4 cat-btn">${category}</button>
+        `
+        categoryContainer.appendChild(div);
+    })
+}
 
 const displayProducts = (products) => {
     const productContainer = document.getElementById("product-container");
@@ -19,12 +70,12 @@ const displayProducts = (products) => {
                     alt=""
                 />
             </div>
-            <div class="flex justify-between px-5 pb-2">
+            <div class="flex items-center justify-between px-5 pb-2">
                 <span
-                    class="bg-primary-content text-primary px-2 py-1 text-xs "
+                    class="bg-primary-content text-primary rounded-full px-2 py-1 text-xs"
                     >${product.category}</span
                 >
-                <span>
+                <span class="text-xs">
                     <i class="fa-solid fa-star text-warning text-xs"></i> ${product.rating.rate} (${product.rating.count})
                 </span>
             </div>
@@ -42,8 +93,10 @@ const displayProducts = (products) => {
             </div>
         </div>
         `
+        manageSpinner(false);
         productContainer.appendChild(div);
     })
 }
 
 loadProducts();
+loadCategories();
